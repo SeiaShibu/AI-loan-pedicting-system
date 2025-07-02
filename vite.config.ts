95cff996f53171990a3a165ba189add7663a7fd1
Seia Shibu <siyashibu.56@gmail.com>
2025-07-02 22:57:07 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/AI-loan-pedicting-system/', // 👈 This should match your repo name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
